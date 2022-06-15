be3a75aa49de21c5411192225f6e6b15bcd392ad
Jignyasajiki <107302707+Jignyasajiki@users.noreply.github.com>
2022-06-15 08:32:16 +0530

    Add files via upload

---
+++ FactorialNumber.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		int i,fact=1;  
+		  int num=5;
+		  for(i=1;i<=num;i++){    
+		      fact=fact*i;    
+		  }    
+		  System.out.println("Factorial of "+num+" is: "+fact);    
+		 }  
+		}  
+
+
+
